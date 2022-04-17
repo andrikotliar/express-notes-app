0deae1046da93ca4b08a76a02b0e223ec1704a97
Andrii Kotliar <andrijkotliar@gmail.com>
2022-04-17 14:33:39 +0300

    Add validations

--- src/routes/notes.ts
+++ src/routes/notes.ts
@@ -7,14 +7,17 @@ import { updateNoteService } from "../services/updateNoteService";
 import { archiveNoteService } from "../services/archiveNoteService";
 import { getStatsService } from "../services/getStatsService";
 
+import { validateRequestService } from "../services/validateRequestService";
+import { NoteSchema, PatchNoteSchema } from "../schemas/notes.schema";
+
 const router = Router();
 
 router.get('/notes', getAllNotesService);
 router.get('/notes/stats', getStatsService);
 router.get('/notes/:id', getOneNoteService);
-router.post('/notes', addNoteService);
+router.post('/notes', validateRequestService(NoteSchema), addNoteService);
 router.delete('/notes/:id', removeNoteService);
-router.patch('/notes/:id', updateNoteService);
+router.patch('/notes/:id', validateRequestService(PatchNoteSchema), updateNoteService);
 router.patch('/notes/archive/:id', archiveNoteService);
 
 export default router;
