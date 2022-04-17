efc3e190656a10faca20aee5d725e301e7672645
Andrii Kotliar <andrijkotliar@gmail.com>
2022-04-17 14:32:24 +0300

    Update note type

--- src/helpers/addStatisticNumber.ts
+++ src/helpers/addStatisticNumber.ts
@@ -1,6 +1,6 @@
-import { NoteType } from "../types/notes.types";
+import { ExtendedNoteType } from "../types/notes.types";
 
-export const addStatisticNumber = (notes: NoteType[], category: string, activeState: boolean) : number => {
+export const addStatisticNumber = (notes: ExtendedNoteType[], category: string, activeState: boolean) : number => {
   const filteredNotes = notes.filter(
     note => note.active === activeState && note.category === category
   );
